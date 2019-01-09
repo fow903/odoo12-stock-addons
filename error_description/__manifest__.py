d98738e9e6918120a4b3a67ba8ff1bfa4dfd7280
edev <edwin2delossantos@gmail.com>
2019-01-09 04:17:22 -0400

    deleting error description

--- error_description/__manifest__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-{
-    'name': "Error_description",
-    'author': "Edwin De Los Santos",
-    'category': 'Uncategorized',
-    'version': '0.1',
-    'depends': ['base','sale'],
-}
