6e92ff7bec08456e15f071711342e7613dff23ce
edev <edwin2delossantos@gmail.com>
2019-01-09 02:21:36 -0400

    deleting unmigrated modules

--- BOM_1/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# -*- coding: utf-8 -*-
-###############################################################################
-#    License, author and contributors information in:                         #
-#    __odoo__.py file at the root folder of this module.                   #
-###############################################################################
-
-from . import models
--- reporte_conduce/models/stock_picking.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models
-
-class stockpicking(models.Model):
-    _inherit = 'stock.picking'
