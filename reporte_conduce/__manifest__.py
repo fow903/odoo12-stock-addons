a27f11f10015ea92ed306b8dd6080c6f2b50a794
klk <edwin2delossantos@gmail.com>
2019-06-14 22:50:05 -0400

    reporte de conduce

--- reporte_conduce/__manifest__.py
+++ reporte_conduce/__manifest__.py
@@ -4,7 +4,8 @@
 'version' : "1.0",
 'author' : "Edwin",
 'depends' : ['stock','shop'],
-'data': ['reports/stock_picking.xml',
+'data': [
+    # 'reports/stock_picking.xml',
          'views/conduce_external_layout.xml'],
 'installable' : True,
 }
